3a44b900bde8ad6e83d7378787c99761bccd7445
jithin <jroy6@asu.edu>
2016-03-05 19:37:56 -0700

    Added contract class

---
+++ app/src/main/java/edu/asu/cse535assgn1/database/AccelerometerContract.java
@@ -0,0 +1,20 @@
+package edu.asu.cse535assgn1.database;
+
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Jithin Roy on 3/5/16.
+ */
+public final class AccelerometerContract {
+
+    public AccelerometerContract() {}
+
+    public static abstract  class AccelerometerEntry implements BaseColumns {
+        public static final String TABLE_NAME = "Accelerometer";
+        public static final String COLUMN_NAME_TIME_STAMP = "timestamp";
+        public static final String COLUMN_NAME_X_VALUE = "xvalue";
+        public static final String COLUMN_NAME_Y_VALUE = "yvalue";
+        public static final String COLUMN_NAME_Z_VALUE = "zvalue";
+    }
+}
